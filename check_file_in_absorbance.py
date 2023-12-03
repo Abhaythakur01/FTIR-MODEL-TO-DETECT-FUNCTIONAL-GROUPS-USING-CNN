5bbe14f315f8a31b7907207c87585a69e608cd01
ABHAY THAKUR <133152272+Abhaythakur01@users.noreply.github.com>
2023-12-03 18:09:30 +0530

    Update check_file_in_absorbance.py

--- check_file_in_absorbance.py
+++ check_file_in_absorbance.py
@@ -1,29 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Nov  8 17:19:16 2020
-
-@author: AbbieEnders
-"""
-
 import jcamp
 import os
 import glob
 import shutil
 import sys
-# This is where your data is coming from and going to
-# The following lines will find all of the files of a given type in the path's folder
+
 
 path = os.path.join(os.getcwd(), sys.argv[1])
 destination = os.path.join(os.getcwd(), sys.argv[2])
 final_dest = os.path.join(os.getcwd(), sys.argv[3])
-# you are probably not moving the py file around, so just change directory to look\touch in correct folder
-# Here are the files that fit your criterion that are within the path file
 
 extension = 'jdx'
 all_files = glob.glob(path + "\*.jdx")
-# read jcampdx file and check absorbance and wavenumbers units
-# return false if not in micrometers (wavenumbers does not equal micrometers = True)
-# return false if not in absorbance (y-units in absorbance = True)
 
 for file in all_files:
     data = jcamp.JCAMP_reader(file)
