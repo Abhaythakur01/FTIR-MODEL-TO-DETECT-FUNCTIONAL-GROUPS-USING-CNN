b122e57f78ecca709b537ae4a30a3bfde4ef07c1
ABHAY THAKUR <133152272+Abhaythakur01@users.noreply.github.com>
2023-12-03 18:22:50 +0530

    Update move_csv_file.py

--- move_csv_file.py
+++ move_csv_file.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 13 12:54:59 2020
-
-@author: AbbieEnders
-"""
-
-# move csv files to new folder
-
 import shutil
 import os
 import sys
@@ -15,6 +6,7 @@ extension = '.csv' # file format
 og = os.path.join(os.getcwd(), sys.argv[1]) # original 
 new = os.path.join(os.getcwd(), sys.argv[2]) # destination
 os.chdir(og) # change directory
+
 # get all of the files in the folder that match the extension
 results = glob.glob('*.{}'.format(extension)) 
 for file in results:
