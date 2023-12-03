5c098adedd27db065da67c65029210f0080f363d
ABHAY THAKUR <133152272+Abhaythakur01@users.noreply.github.com>
2023-12-03 18:21:43 +0530

    Update make_functional_group_directory.py

--- make_functional_group_directory.py
+++ make_functional_group_directory.py
@@ -1,14 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Nov 16 19:41:52 2020
-
-@author: AbbieEnders
-"""
-
 import os
 import sys
 import shutil
+
 # directory to create inside the original directory
+
 target_dir = os.path.join(os.getcwd(), sys.argv[2], sys.argv[1])
 move_dir1 = os.path.join(os.getcwd(), sys.argv[2], sys.argv[3])
 move_dir2 = os.path.join(os.getcwd(), sys.argv[2], sys.argv[2])
